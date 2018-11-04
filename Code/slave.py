ed4a0aed9f37272f0cef7b3446d9bfcaacd997e0
Ryan Malthaner <ryanm@fourier.math.tamu.edu>
2018-11-03 22:58:19 -0500

    Added ability to normalize vectors with inf_norm.py

--- Code/slave.py
+++ Code/slave.py
@@ -1,10 +1,11 @@
 import sys
 import ast
 import MPCModel
+import datetime
 
-print('Starting model number ' + sys.argv[2])
+print('Starting model number ' + sys.argv[2] + ' at time ' + str(datetime.datetime.now()))
 sys.stdout.flush()
 MPCModel.generate_model(ast.literal_eval(sys.argv[1]))
-print('Finished model number ' + sys.argv[2])
+print('Finished model number ' + sys.argv[2] + ' at time ' + str(datetime.datetime.now()))
 sys.stdout.flush()
 
